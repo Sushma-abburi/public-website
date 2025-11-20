77348f46cd6c08cfd72e7faea93867290e720e99
Sushma-abburi <sushma.a@dhatvibs.com>
2025-11-20 15:53:48 +0530

    updated code

--- config/db.js
+++ config/db.js
@@ -10,20 +10,53 @@
 // };
 
 // module.exports = connectDB;
+// const mongoose = require("mongoose");
+
+// let isConnected = false;
+
+// const connectDB = async () => {
+//   if (isConnected) return;
+
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     isConnected = true;
+//     console.log("MongoDB Connected");
+//   } catch (err) {
+//     console.log("MongoDB Connection Error:", err.message);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
-let isConnected = false;
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  if (isConnected) return;
+  if (cached.conn) return cached.conn; // reuse connection
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000,
+    };
+
+    cached.promise = mongoose
+      .connect(process.env.MONGO_URI, opts)
+      .then((mongoose) => mongoose);
+  }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    isConnected = true;
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.log("MongoDB Connection Error:", err.message);
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
   }
+
+  return cached.conn;
 };
 
 module.exports = connectDB;
