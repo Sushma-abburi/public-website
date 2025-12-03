6bfc0d12d5f592a6213fdf981f2787f0ccef8146
Sushma-abburi <sushma.a@dhatvibs.com>
2025-12-03 12:00:18 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -1,37 +1,12 @@
-// const mongoose = require("mongoose");
-
-// mongoose.set("strictQuery", false);
-// mongoose.set("autoIndex", true);  // enables index creation
-// let isConnected = false; // prevent multiple connections
-
-// const connectDB = async () => {
-//   if (isConnected) return;
-
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGO_URI, {
-//       serverSelectionTimeoutMS: 5000,
-//     });
-
-//     console.log("MongoDB connected");
-//     isConnected = true;
-//     return conn;
-//   } catch (err) {
-//     console.error("MongoDB connection error:", err.message);
-//     throw err;
-//   }
-// };
-
-// module.exports = connectDB;
-
-
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 mongoose.set("bufferTimeoutMS", 20000);
 mongoose.set("strictQuery", false);
 mongoose.set("autoIndex", true);
+
 let isConnected = false;
 
-export const connectDB = async () => {
+const connectDB = async () => {
   if (isConnected) {
     return;
   }
@@ -51,3 +26,4 @@ export const connectDB = async () => {
   }
 };
 
+module.exports = connectDB;
