c6a824b9d8ffe8ea77c6f75115bd96b31a40855e
Sushma-abburi <sushma.a@dhatvibs.com>
2025-12-03 11:55:56 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -1,24 +1,53 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
+// mongoose.set("strictQuery", false);
+// mongoose.set("autoIndex", true);  // enables index creation
+// let isConnected = false; // prevent multiple connections
+
+// const connectDB = async () => {
+//   if (isConnected) return;
+
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       serverSelectionTimeoutMS: 5000,
+//     });
+
+//     console.log("MongoDB connected");
+//     isConnected = true;
+//     return conn;
+//   } catch (err) {
+//     console.error("MongoDB connection error:", err.message);
+//     throw err;
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+import mongoose from "mongoose";
+
+mongoose.set("bufferTimeoutMS", 20000);
 mongoose.set("strictQuery", false);
-mongoose.set("autoIndex", true);  // enables index creation
-let isConnected = false; // prevent multiple connections
+mongoose.set("autoIndex", true);
+let isConnected = false;
 
-const connectDB = async () => {
-  if (isConnected) return;
+export const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 15000,
     });
 
-    console.log("MongoDB connected");
     isConnected = true;
-    return conn;
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-    throw err;
+    console.log("✅ MongoDB Connected:", conn.connection.host);
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error.message);
+    throw error;
   }
 };
 
-module.exports = connectDB;
